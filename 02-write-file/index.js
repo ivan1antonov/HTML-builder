61b9dd961bb2263c3e7b60800d43c78212268e78
Ivan Antonov <ivan1antonov@gmail.com>
2024-04-28 14:47:45 +0400

    feat: add solution for task2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const { stdin, stdout, exit } = process;
+const { stdin, stdout, stderr } = process;
 const context = path.join(__dirname, 'text.txt');
-const streamWrite = fs.createWriteStream(context);
+const output = fs.createWriteStream(context);
 
 stdout.write('Hi, enter anything word for write in the file \n');
-stdin.on('data', (chunk) => streamWrite.write(chunk));
-stdin.on('error', (error) => stdout.write('All was write in file'));
+stdin.on('data', (chunk) => {
+  if (chunk.toString().includes('exit')) process.exit();
+  output.write(chunk);
+});
+process.on('exit', () => stdout.write('Everything was writing'));
+process.on('SIGINT', () => process.exit());
