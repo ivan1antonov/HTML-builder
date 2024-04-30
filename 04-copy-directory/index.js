4fc21c3d3cd3e502d1eabbb8f247c3cd9127accf
Ivan Antonov <ivan1antonov@gmail.com>
2024-04-30 12:09:18 +0400

    feat: add solution task 4

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,30 @@
+const fsPromise = require('fs').promises;
+const path = require('path');
+const sourceDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+  try {
+    await fsPromise.rm(destDir, { force: true, recursive: true });
+    await fsPromise.mkdir(destDir, { recursive: true }, () => {
+      console.log('Folder created');
+    });
+    const copyDir = await fsPromise.readdir(
+      sourceDir,
+      { withFileTypes: true },
+      (files) => {
+        console.log(files);
+        return files;
+      },
+    );
+    copyDir.forEach((file) => {
+      const fileSourceName = path.join(sourceDir, file.name);
+      const fileDestName = path.join(destDir, file.name);
+      fsPromise.copyFile(fileSourceName, fileDestName);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+copyDir();
