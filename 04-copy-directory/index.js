fe561d39aad64b2a789ffe92e417a50cec8cffae
Ivan Antonov <ivan1antonov@gmail.com>
2024-04-30 19:58:43 +0400

    feat: add solution task 5

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,17 +6,8 @@ const destDir = path.join(__dirname, 'files-copy');
 async function copyDir() {
   try {
     await fsPromise.rm(destDir, { force: true, recursive: true });
-    await fsPromise.mkdir(destDir, { recursive: true }, () => {
-      console.log('Folder created');
-    });
-    const copyDir = await fsPromise.readdir(
-      sourceDir,
-      { withFileTypes: true },
-      (files) => {
-        console.log(files.toString());
-        return files;
-      },
-    );
+    await fsPromise.mkdir(destDir, { recursive: true });
+    const copyDir = await fsPromise.readdir(sourceDir, { withFileTypes: true });
     copyDir.forEach((file) => {
       const fileSourceName = path.join(sourceDir, file.name);
       const fileDestName = path.join(destDir, file.name);
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const pathSource = path.join(__dirname, 'styles');
+const bundle = path.join(__dirname, 'project-dist', 'bundle.css');
+const writeStream = fs.createWriteStream(bundle);
+
+fs.readdir(pathSource, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  files.forEach((file) => {
+    const extName = path.extname(file.name);
+    if (file.isFile() && extName === '.css') {
+      const filePath = path.join(pathSource, file.name);
+      const readStream = fs.createReadStream(filePath);
+
+      readStream.pipe(writeStream);
+    }
+  });
+  // const cssFiles = files.filter((file) => {
+  //   return file.isFile() && extName === '.css';
+  // });
+  // cssFiles.forEach((file) => {
+  // });
+});
